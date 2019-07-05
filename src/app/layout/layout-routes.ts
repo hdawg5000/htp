6b86d80c4d67303dae4676bcff41e5dc7680b4e6
Hammad Naseem <hammadnsm@gmail.com>
2019-07-05 09:01:48 -0500

    added home and contact components

---
+++ src/app/layout/layout-routes.ts
@@ -0,0 +1,12 @@
+import { Routes } from '@angular/router';
+import { LayoutComponent } from './layout.component';
+
+export const routes: Routes = [
+    {
+        path: '', component: LayoutComponent,
+        children: [
+            { path: '', loadChildren: './home/home.module#HomeModule' },
+            { path: 'contact', loadChildren: './contact/contact.module#ContactModule' }
+        ]
+    }
+];
---
+++ src/app/layout/layout.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+import { routes } from './layout-routes';
+import { RouterModule } from '@angular/router';
+import { LayoutComponent } from './layout.component';
+
+@NgModule({
+    declarations: [
+        LayoutComponent
+    ],
+    imports: [
+        CommonModule,
+        RouterModule.forChild(routes)
+    ]
+})
+export class LayoutModule { }
