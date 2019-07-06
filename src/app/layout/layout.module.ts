268bf7c5a55b7bc7fad764dafbd40d5bb02103cb
Hammad Naseem <hammadnsm@gmail.com>
2019-07-05 20:45:51 -0500

    Added routerlink to navbar

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,15 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HomeComponent } from './home/home.component';
 import { routes } from './layout-routes';
 import { RouterModule } from '@angular/router';
 import { LayoutComponent } from './layout.component';
-import { AboutComponent } from './about/about.component';
 
 @NgModule({
     declarations: [
-        LayoutComponent,
-        AboutComponent
+        LayoutComponent
     ],
     imports: [
         CommonModule,
--- src/shared/header/header.module.ts
+++ src/shared/header/header.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header.component';
+import { RouterModule } from '@angular/router';
+import { routes } from 'src/app/layout/home/home-routes';
 
 @NgModule({
     declarations: [
         HeaderComponent
     ],
     imports: [
-        CommonModule
+        CommonModule,
+        RouterModule.forChild(routes)
     ],
     exports: [
         HeaderComponent
