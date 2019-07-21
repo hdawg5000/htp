bd67909dfd2a886873422bcb1d7c87b5bea1f975
Hammad Naseem <hammadnsm@gmail.com>
2019-07-21 15:57:11 -0500

    Moved all images to images folder

--- src/app/layout/home/home.component.ts
+++ src/app/layout/home/home.component.ts
@@ -16,16 +16,16 @@ export class HomeComponent implements OnInit {
   public showNavigationArrows = false
   public showNavigationIndicators = false
   public images = [
-    ['assets/man_on_a_boat.jpg', 'Man on a boat'],
-    ['assets/man_on_tracks.jpg', 'Man on tracks'],
-    ['assets/jars_on_a_table.jpg', 'Jars on a table'],
-    ['assets/two_men_and_baby.jpg', 'Two men and a baby']
+    ['assets/images/man_on_a_boat.jpg', 'Man on a boat'],
+    ['assets/images/man_on_tracks.jpg', 'Man on tracks'],
+    ['assets/images/jars_on_a_table.jpg', 'Jars on a table'],
+    ['assets/images/two_men_and_baby.jpg', 'Two men and a baby']
   ]
   public mobileImages = [
-    ['assets/boat_in_venice.jpg', 'Boat in Venice'],
-    ['assets/cake.jpg', 'Birthday cake'],
-    ['assets/railroad_tracks.jpg', 'Railroad tracks'],
-    ['assets/venice.jpg', 'Venice']
+    ['assets/images/boat_in_venice.jpg', 'Boat in Venice'],
+    ['assets/images/cake.jpg', 'Birthday cake'],
+    ['assets/images/railroad_tracks.jpg', 'Railroad tracks'],
+    ['assets/images/venice.jpg', 'Venice']
   ]
 
   @HostListener('window:resize', ['$event'])
